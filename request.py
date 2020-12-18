2031414a8010c37f0b361b5cccddd5648ebf82af
ZenVega <ufogreeks@gmail.com>
2020-12-18 17:30:08 +0100

    request.py returns more and normalized data

--- request.py
+++ request.py
@@ -3,7 +3,7 @@ from myAPI import API_Key
 import json
 
 service = build('youtube', 'v3', developerKey=API_Key)
-vidID = "m0RWSHdS77E"
+vidID = "_1M1rhO5rXo"
 
 
 # MAKES A GET REQUEST AND RETURNS A DICT/JSON
@@ -23,27 +23,88 @@ def get_vid_data(vidID):
         json.dump(json_object, outfile)
     return json_object
 
-# FILTERS RELEANT DATA OUT OF DICT AND RETURNS ARRAY WITH COMMENTS
 
+# FILTERS RELEANT DATA OUT OF DICT AND RETURNS NEW DICT
 
 def filter_for_comments(json):
-    comments = []
+    videoID = json["items"][0]["snippet"]["videoId"]
+
+    # CREATE EMPTY DICT
+    data = {
+        "comments_by_ID": {},
+        "commentIDs": [],
+        "authors_by_ID": {},
+        "authorIDs": []
+    }
+
+    # ITERATE OVER COMMENTS
     for item in json['items']:
+
+        # GET RELEVANT DATA
         commentID = item['id']
-        comment = item["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
-        author = item["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"]
+        comment_txt = item["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
+        author_name = item["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"]
+        author_ID = item["snippet"]["topLevelComment"]["snippet"]["authorChannelId"]["value"]
+        timestamp = item["snippet"]["topLevelComment"]["snippet"]["updatedAt"]
+        top_level = True
 
-        comments.append({
-            commentID: {
-                'autor': author,
-                'comment': comment
+        # FILL IN THE OUTPUT DICT
+        data["comments_by_ID"][commentID] = {
+            'author_ID': author_ID,
+            'commetn_txt': comment_txt,
+            'timestamp': timestamp,
+            'top_level': top_level,
+            'videoID': videoID
+        }
+
+        data["commentIDs"].append(commentID)
+
+        if not "author_ID" in data["authors_by_ID"]:
+            data["authors_by_ID"][author_ID] = {
+                "autor_name": author_name,
+                'author_ID': author_ID
             }
-        })
+            data["authorIDs"].append(author_ID)
 
-    return comments
+        # IF THERE ARE REPLIES ADD THESE TO THE EXPORT DATA
+        if "replies" in item:
+            counter = 0
+            for reply in item["replies"]["comments"]:
+
+                # REPLY COMMENTS HAVE ADDITIONAL KEYS reply_no & parent_comment
+                parent_comment = commentID
+                top_level = False
+                reply_no = counter
+                counter += 1
+                commentID = reply["id"]
+                comment_txt = reply["snippet"]["textOriginal"]
+                author_name = reply["snippet"]["authorDisplayName"]
+                author_ID = reply["snippet"]["authorChannelId"]["value"]
+                timestamp = reply["snippet"]["updatedAt"]
+
+                data["comments_by_ID"][commentID] = {
+                    'author_ID': author_ID,
+                    'commetn_txt': comment_txt,
+                    'timestamp': timestamp,
+                    'top_level': top_level,
+                    'videoID': videoID,
+                    'reply_no': reply_no,
+                    'parent_comment': parent_comment
+                }
+
+                data["commentIDs"].append(commentID)
+
+                if not "author_ID" in data["authors_by_ID"]:
+                    data["authors_by_ID"][author_ID] = {
+                        "autor_name": author_name,
+                        'author_ID': author_ID
+                    }
+                    data["authorIDs"].append(author_ID)
+
+    return data
+
 
 # CREATES ENTRY AND EXPORTS IT TO FILE
-
 
 def create_entry(vidID):
     json = get_vid_data(vidID)
