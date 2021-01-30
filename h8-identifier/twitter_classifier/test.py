0706284be1ed90104586fc80471a201022d13b64
ZenVega <ufogreeks@gmail.com>
2021-01-30 10:51:02 +0100

    rename h8-identifier to h8_identifier

--- h8-identifier/twitter_classifier/test.py
+++
@@ -1,3 +0,0 @@
-from url_to_hate import get_prediction
-
-print(get_prediction("https://www.youtube.com/watch?v=6eGEX_LTqhQ"))
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='h8-identifier',
-    packages=['h8-identifier'],
+    name='h8_identifier',
+    packages=['h8_identifier'],
     include_package_data=True,
     install_requires=[
         'flask',
