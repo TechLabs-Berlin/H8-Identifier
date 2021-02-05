9cf46b252659d8003621517b73343d769d341f5e
ZenVega <ufogreeks@gmail.com>
2021-02-05 22:38:09 +0100

    replace dummy data

--- h8_identifier/pipeline.py
+++ h8_identifier/pipeline.py
@@ -1,20 +1,21 @@
 import random
-from .yt_request import get_title_and_description
+from .yt_request import get_data_for_page
 from .twitter_classifier.url_to_hate import get_prediction
 
 
 def generate_output(id):
 
     comments, count_hate, count, percentage = get_prediction(id)
-    title, description, chanel_title = get_title_and_description(id)
+    title, description, chanel_title, views, commentCount, likes, dislikes = get_data_for_page(
+        id)
 
     data_dict = {
         "title": title,
         "description": description,
         "chanel_title": chanel_title,
-        "number_of_comments": count,
+        "number_of_comments": commentCount,
         "percent_hate": percentage,
-        "views": random.randint(1, 100001),
+        "views": views,
         "shared": random.randint(1, 100001),
         "comments": comments
     }
--- h8_identifier/yt_request.py
+++ h8_identifier/yt_request.py
@@ -20,8 +20,17 @@ def get_id_from_url(url):
     return id
 
 
-def get_title_and_description(id):
-    data = get_vid_data(id)
+def get_data_for_page(id):
+    meta_data, activity_data = get_vid_data(id)
+    title, description, chanel_title = get_title_and_description(meta_data)
+    views, commentCount, likes, dislikes = get_views_commentCount_likes(
+        activity_data)
+
+    return title, description, chanel_title, views, commentCount, likes, dislikes
+
+
+def get_title_and_description(meta_data):
+    data = meta_data
     title = data["items"][0]["snippet"]["title"]
     description = data["items"][0]["snippet"]["description"]
     chanel_title = data["items"][0]["snippet"]["channelTitle"]
@@ -34,6 +43,25 @@ def get_vid_data(vidID):
         f'https://www.googleapis.com/youtube/v3/videos?key={APIkey}&part=snippet&id={vidID}')
     data_bytes = data.content
     data_json = data_bytes.decode('utf8')
-    json_object = json.loads(data_json)
+    meta_data = json.loads(data_json)
 
-    return json_object
+    act_data = requests.get(
+        f'https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics&id={vidID}&key={APIkey}'
+    )
+    act_data_bytes = act_data.content
+    act_data_bytes = act_data_bytes.decode('utf8')
+    activity_data = json.loads(act_data_bytes)
+
+    return meta_data, activity_data
+
+
+def get_views_commentCount_likes(activity_data):
+
+    statistics = activity_data["items"][0]["statistics"]
+    views = int(statistics["viewCount"])
+    commentCount = int(statistics["commentCount"])
+    likes = int(statistics["likeCount"])
+    dislikes = int(statistics["dislikeCount"])
+    print(type(commentCount))
+
+    return views, commentCount, likes, dislikes
