d0cc905dc574fc98c2fc98b0bb4745f5c381b686
ZenVega <ufogreeks@gmail.com>
2021-02-05 16:10:52 +0100

    import service from url_to_hate delete doubled functionality

--- h8_identifier/yt_request.py
+++ h8_identifier/yt_request.py
@@ -1,13 +1,11 @@
 from googleapiclient.discovery import build
-from .twitter_classifier.url_to_hate import filter_for_comments
+from .twitter_classifier.url_to_hate import filter_for_comments, service
 import json
 import requests
 import os
 import random
 
 APIkey = os.environ['APIkey']
-
-service = build('youtube', 'v3', developerKey=APIkey)
 
 
 # get vid id from URL-string
@@ -39,21 +37,3 @@ def get_vid_data(vidID):
     json_object = json.loads(data_json)
 
     return json_object
-
-
-# MAKES A GET REQUEST AND RETURNS A DICT/JSON
-
-def get_comment_data(vidID):
-    request = service.commentThreads().list(
-        part="snippet, replies",
-        videoId=vidID,
-        maxResults=100
-    )
-    response = request.execute()
-    return response
-
-
-def create_entry(vidID):
-    json = get_comment_data(vidID)
-    comments = filter_for_comments(json)
-    return comments
